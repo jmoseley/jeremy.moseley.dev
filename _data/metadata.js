8ec29ca7b26b910f1ba9b187b8e51111a7900e27
Jeremy Moseley <jeremy@moseley.dev>
2024-02-27 20:59:55 -0800

    blog whoo

---
+++ _data/metadata.js
@@ -0,0 +1,12 @@
+module.exports = {
+	title: "Ramblings of a Developer",
+	url: "https://jeremy.moseley.dev/",
+	language: "en",
+	description:
+		"A blog about software development, building stuff, and other things I find interesting.",
+	author: {
+		name: "Jeremy Moseley",
+		email: "jeremy@moseley.dev",
+		url: "https://jeremy.moseley.dev/about-me/",
+	},
+};
---
+++ content/about/index.md
@@ -0,0 +1,10 @@
+---
+layout: layouts/base.njk
+eleventyNavigation:
+  key: About Me
+  order: 3
+---
+
+# About Me
+
+I am a person that writes stuff. I also do other things. Sometimes I write about those things. Sometimes I don't.
